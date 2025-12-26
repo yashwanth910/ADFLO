31308adb9e0668683cafcaac877c7ef57b91cfac
yashwanth910 <wanth404@gmail.com>
2025-12-27 01:12:54 +0530

    message fix again

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -30,9 +30,11 @@ const Contact = () => {
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        apikey: import.meta.env.VITE_SUPABASE_ANON_KEY,
-      },
+  "Content-Type": "application/json",
+  "Authorization": `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+  "apikey": import.meta.env.VITE_SUPABASE_ANON_KEY,
+},
+
       body: JSON.stringify(data),
     }
   );
